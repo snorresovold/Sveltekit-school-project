9f508b12545c1f32fb63ab0d0ddeb9672712e0ae
Snorre Søvold <73354016+snorresovold@users.noreply.github.com>
2023-03-13 09:28:04 +0100

    updates|

---
+++ svelte-school/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Todo from "../lib/Todo.svelte";
+    let todos = ["Rydde", "hallo"]
+    let add_item: string = "";
+    function add(x: string) {
+        todos.push(x)
+        todos = todos;
+    }
+    function remove(x: number) {
+        todos.splice(x, 1)
+        todos = todos;
+    }
+</script>
+
+
+<h1>Todos</h1>
+<input bind:value={add_item}>
+<button on:click={() => add(add_item)}>Add todo</button>
+
+
+{#each todos as todo, i}
+<br/>
+    <button on:click={() => remove(i)}>Delete {todo}</button>
+    <Todo name={todo} />
+{/each}
